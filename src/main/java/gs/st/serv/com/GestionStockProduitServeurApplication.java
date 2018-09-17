d03850a6eeef2a3193d61f3063312b87eb24cdaa
ayoub <ayoub.boudad91@gmail.com>
2018-09-17 20:46:58 +0000

    first commit

---
+++ src/main/java/gs/st/serv/com/GestionStockProduitServeurApplication.java
@@ -0,0 +1,12 @@
+package gs.st.serv.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockProduitServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockProduitServeurApplication.class, args);
+	}
+}
